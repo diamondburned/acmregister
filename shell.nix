61c3022b79768261dac17f7bb52a89c8c6fbe503
diamondburned <diamond@arikawa-hi.me>
2024-01-02 20:05:25 -0800

    Don't bother locking Nixpkgs for shell

--- shell.nix
+++ shell.nix
@@ -1,35 +1,18 @@
-{ systemPkgs ? import <nixpkgs> {} }:
-
-let lib = systemPkgs.lib;
-	overlay = self: super: {
+let
+	pkgsOverlay = self: super: {
 		go = super.go_1_19;
 	};
-	pkgs = import (systemPkgs.fetchFromGitHub {
-		owner = "NixOS";
-		repo  = "nixpkgs";
-		rev   = "27ccd29078f974ddbdd7edc8e38c8c8ae003c877";
-		hash  = "sha256:1lsjmwbs3nfmknnvqiqbhh103qzxyy3z1950vqmzgn5m0zx7048h";
-	}) {
-		overlays = [ overlay ];
-	};
+in
 
-	sqlc = pkgs.buildGoModule rec {
-		name = "sqlc";
-		version = "1.17.2";
+{
+	systemPkgs ? import <nixpkgs> {
+		overlays = [ pkgsOverlay ];
+	}
+}:
 
-		src = pkgs.fetchFromGitHub {
-			owner  = "kyleconroy";
-			repo   = "sqlc";
-			rev    = "v${version}";
-			sha256 = "0kq9f605pwjvs2azc5bq6k6iji54yp1dmsnrb7lfj2rvilb4hiyz";
-		};
-
-		proxyVendor = true;
-		vendorSha256 = "0ih9siizn6nkvm4wi0474wxy323hklkhmdl52pql0qzqanmri4yb";
-
-		# Skip fuzz tests.
-		doCheck = false;
-	};
+let
+	pkgs = systemPkgs;
+	lib = systemPkgs.lib;
 
 	sql-formatter = systemPkgs.writeShellScriptBin "sql-formatter" ''
 		set -e
